1c10c2441bf6e3da7cecfe92762c92537cffe201
steevansan <52646604+steevansan@users.noreply.github.com>
2025-06-14 00:07:59 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)  # This makes it work from root directory
+
+# Your Portfolio class and routes here...
+
+if __name__ == "__main__":
+    app.run()
